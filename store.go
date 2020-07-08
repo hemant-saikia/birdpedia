48487860cf438c6f9dd99eaa8e372d293fa15773
saikiahemant <hmnt.ska@gmail.com>
2020-07-08 14:30:33 +0530

    APIs, Tests, Persistence in memory

---
+++ store.go
@@ -0,0 +1,14 @@
+package main
+
+import  (
+	"database/sql"
+)
+
+// Our store will have two methods, to add a new bird,
+// and to get all existing birds
+// Each method returns an error, in case something goes wrong
+type Store interface {
+	CreateBird(bird *Bird) error
+	GetBirds() ([]*Bird, error)
+}
+
